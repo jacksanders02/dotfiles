e05e38b9db6f48240ab403d3828724a7538aefd7
Jack Sanders <personal@jacksanders.uk>
2024-11-20 11:14:48 +0000

    feat(pacakges): added tmux, insomnia, and updated deps list script

--- generate_deps_list.sh
+++ generate_deps_list.sh
@@ -1,2 +1,4 @@
-pacman -Qe |cut -f 1 -d " " > packages.txt
+pacman -Qe |cut -f 1 -d " " > temp.txt
+comm -12 <(pacman -Slq | sort) <(sort temp.txt) | tee packages.txt > /dev/null
+rm temp.txt
 pacman -Qm |cut -f 1 -d " " > packages-aur.txt
